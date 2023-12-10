e82425c71ed0e7bbf8755d17c36161103688c0de
rom341 <roman.murzik.antpnov@gmail.com>
2023-12-10 12:42:11 +0400

    First commit

---
+++ TG_WITH_MAFIA_bot/TG_WITH_MAFIA_bot/BotController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace TG_WITH_MAFIA_bot
+{
+    public class BotController
+    {
+        private string botToken = "6984398334:AAFamXOnYYfZ9dkxFw0D6Zu1aqdq0QW23Ro";
+
+        public void StartBot()
+        {
+            Console.WriteLine("Start working");
+            var client = new TelegramBotClient(botToken);
+            client.StartReceiving(Update, Error);
+        }
+
+        async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+            if (message.Text != null)
+            {
+                await botClient.SendTextMessageAsync(message.Chat.Id, $"Bot is working!");               
+            }
+        }
+        private static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TG_WITH_MAFIA_bot/TG_WITH_MAFIA_bot/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TG_WITH_MAFIA_bot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BotController controller = new BotController();
+            controller.StartBot();
+            Console.ReadLine();
+        }
+    }
+}
