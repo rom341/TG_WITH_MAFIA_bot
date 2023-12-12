d401cdd666f859f6920aa1b7050785122c866049
rom341 <roman.murzik.antpnov@gmail.com>
2023-12-12 14:11:13 +0400

    Menu added

--- TG_WITH_MAFIA_bot/TG_WITH_MAFIA_bot/BotController.cs
+++ TG_WITH_MAFIA_bot/TG_WITH_MAFIA_bot/BotController.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Telegram.Bot.Types;
 using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TG_WITH_MAFIA_bot
 {
@@ -16,21 +16,57 @@ namespace TG_WITH_MAFIA_bot
         public void StartBot()
         {
             Console.WriteLine("Start working");
-            var client = new TelegramBotClient(botToken);
-            client.StartReceiving(Update, Error);
+            var botClient = new TelegramBotClient(botToken);
+            botClient.StartReceiving(Update, Error);
         }
 
         async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
         {
-            var message = update.Message;
-            if (message.Text != null)
+            if (update.Type == UpdateType.Message)
             {
-                await botClient.SendTextMessageAsync(message.Chat.Id, $"Bot is working!");               
+                var message = update.Message;
+
+                if (message.Text != null)
+                {
+                    if (message.Text == "/start")
+                    {
+                        await SendMainMenu(botClient, message.Chat.Id);
+                    }
+                }
+            }
+            else if (update.Type == UpdateType.CallbackQuery)
+            {
+                var callbackQuery = update.CallbackQuery;
+                var chatId = callbackQuery.Message.Chat.Id;
+
+                if (callbackQuery.Data == "/BTN_CREATE")
+                {
+                    await botClient.SendTextMessageAsync(chatId, $"Вы выбрали 'Создать комнату'");
+                }
+                else if (callbackQuery.Data == "/BTN_CONNECT")
+                {
+                    await botClient.SendTextMessageAsync(chatId, $"Вы выбрали 'Присоединиться'");
+                }
             }
         }
+
         private static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
         {
             throw new NotImplementedException();
+        }
+
+        private static async Task SendMainMenu(ITelegramBotClient botClient, long chatId)
+        {
+            var keyboard = new InlineKeyboardMarkup(new[]
+            {
+                new []
+                {
+                    InlineKeyboardButton.WithCallbackData("Создать комнату", "/BTN_CREATE"),
+                    InlineKeyboardButton.WithCallbackData("Присоединиться", "/BTN_CONNECT")
+                }
+            });
+
+            await botClient.SendTextMessageAsync(chatId, "Выберите действие:", replyMarkup: keyboard);
         }
     }
 }
